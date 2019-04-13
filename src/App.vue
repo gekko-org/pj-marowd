80dbcf136b1c0a2e543970067b2a7902668a268b
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-04-13 20:21:57 +0900

    add: ShowCase.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <show-case/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShowCase from "@/components/ShowCase";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ShowCase,
   }
 }
 </script>
---
+++ src/components/ShowCase.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        This is ShowCase
+    </p>
+</template>
+
+<script>
+    export default {
+        name: "ShowCase"
+    }
+</script>
+
+<style scoped>
+
+</style>
