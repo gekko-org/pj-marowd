44deb2761f75ef5497de6ca7043516c948e01d5d
reiji <reiji-kobayashi@reud.net>
2019-04-27 14:49:41 +0900

    add: AppEvaluartionItem.vue

---
+++ src/components/AppEvaluationItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <star-rating></star-rating>
+</template>
+
+<script>
+    import StarRating from "vue-star-rating/src/star-rating";
+    export default {
+        name: "AppEvaluationItem",
+        components: {StarRating}
+    }
+</script>
+
+<style scoped>
+
+</style>
