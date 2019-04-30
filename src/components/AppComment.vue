cf345fe450d7ede89edf4fdc075b9446e48ee4c5
kzmaro <maronkazu94@gmail.com>
2019-04-30 22:07:36 +0900

    appcoment 2.1 諸々の修正

--- src/components/AppComment.vue
+++ src/components/AppComment.vue
@@ -1,23 +1,26 @@
 <template>
-    <v-container grid-list-md>
+    <v-container grid-list-lg>
         <v-flex xs10 sm7>
             <v-card>
                 <v-card-title class="font-weight-bold pb-0">{{subject}}</v-card-title>
                 <v-card-text class='pb-0'>
-                    <v-layout row>
-                        <v-flex xs1 d-flex><img src="../assets/loginphoto.png"></v-flex>
-                        <v-layout justify-center>
-                            <v-flex xs8 d-flex>{{date}}<br>reviewed by {{name}}</v-flex>
-                        </v-layout>
+                    <v-layout wrap align-light>
+                        <v-flex xs2 sm1 class='pr-5'><img :src="loadImg()"></v-flex>
+                        <!--<v-layout justify-center>-->
+                        <v-flex xs8 sm6>
+                            <div>{{date}}<br>reviewed by {{name}}</div>
+                        </v-flex>
+                        <!--</v-layout>-->
+
+                        <v-flex xs12 sm3 d-flex class='pt-0'>
+                            <button class='goodstyle' v-bind:class='{active:goodIsActive}'
+                                    v-on:click='goodIsActive=!goodIsActive;changeStateGood()'>
+                                <i class='fas fa-thumbs-up'></i></button>&nbsp;&nbsp;&nbsp;
+                            <button class='badstyle' v-bind:class='{active:badIsActive}'
+                                    v-on:click='badIsActive=!badIsActive;changeStateBad()'>
+                                <i class='fas fa-thumbs-down' v-bind:class='{active:badIsActive}'></i></button>
+                        </v-flex>
                     </v-layout>
-                    <v-flex xs10>
-                        <button class='goodstyle' v-bind:class='{active:goodIsActive}'
-                                v-on:click='goodIsActive=!goodIsActive;changeStateGood()'>
-                            <i class='fas fa-thumbs-up'></i></button>&nbsp;&nbsp;&nbsp;
-                        <button class='badstyle' v-bind:class='{active:badIsActive}'
-                                v-on:click='badIsActive=!badIsActive;changeStateBad()'>
-                            <i class='fas fa-thumbs-down' v-bind:class='{active:badIsActive}'></i></button>
-                    </v-flex>
                 </v-card-text>
                 <v-card-text>
                     {{text}}
@@ -29,23 +32,53 @@
 <script>
     export default {
         name: "AppComment",
-        props: ['name', 'date', 'subject', 'text',],
+        // props:['name','date','text','subject','like'],
+        props: {
+            name: {
+                type: String,
+                default: 'default'
+            },
+            image: {
+                type: String,
+                isRequired: true,
+                // default: '../assets/loginphoto.png'
+            },
+            date: {
+                type: String,
+                default: 'XXXX.XX.XX'
+            },
+            subject: {
+                type: String,
+                default: 'default'
+            },
+            text: {
+                type: String,
+                default: 'defalut'
+            },
+        },
         data: function () {
             return {
                 goodIsActive: false,
                 badIsActive: false,
+                // image_src: require(`../../static/${this.image}`),
             }
         },
-        methods: {
-            changeStateGood: function () {
-                this.$emit('buttonPush',);
-                this.badIsActive = false;
-            },
-            changeStateBad: function () {
-                this.$emit('buttonPush',);
-                this.goodIsActive = false;
+            methods: {
+            //like=1でlike,like=0でdislikeを表す
+                changeStateGood: function () {
+                    this.like=1;
+                    this.$emit('buttonPush',this.like);
+                    this.badIsActive = false;
+                },
+                changeStateBad: function () {
+                    this.like=0;
+                    this.$emit('buttonPush',this.like);
+                    this.goodIsActive = false;
+                },
+                loadImg: function(){
+                    return require(`../../static/loginphoto.png`);
+                },
             }
-        }
     }
 </script>
 
@@ -53,7 +86,7 @@
 <style scoped>
     .goodstyle {
         font-size: 170%;
-        padding: 8px;
+        padding: 5px;
         color: gray;
         cursor: pointer;
     }
@@ -65,7 +98,7 @@
 
     .badstyle {
         font-size: 170%;
-        padding: 8px;
+        padding: 5px;
         color: gray;
         cursor: pointer;
     }
--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -5,11 +5,11 @@
             <app-filter-search @input="EventTest"></app-filter-search>
         </p>
         <p>
-            <app-good-button fav=121 @favPush="check" @favUnPush="check"></app-good-button>
+            <app-good-button fav=121 @favPush="EventTest" @favUnPush="EventTest"></app-good-button>
         </p>
         <p>
-            <app-comment @buttonPush="check" @buttonUnPush="check" name=kzmaro subject="この授業は絶対に取るべき！！！"
-                         date="2019.04.08" text="この授業は落胆でありながら出席確認がなく、どんなに遅刻しても先生は許してくれる髪授業です。しかし注意しなければならないことが１つだけあります。
+            <app-comment like=0 @buttonPush="EventTest" @buttonUnPush="EventTest" name='kzmaro' image="loginphoto.png" subject="この授業は絶対に取るべき！！！"
+                         date="2019.04.08" text="この授業は楽単でありながら出席確認がなく、どんなに遅刻しても先生は許してくれる神授業です。しかし注意しなければならないことが１つだけあります。
                     それは小テストです。この授業では１タームを通して３回の小テストが行われます。この小テストの日程はシラバスにも載っていませんし、初回授業などでもアナウンスされません。つまり、このテスト日程を知るためには、授業に出なければいけないのです。"></app-comment>
             <app-evaluation-item
                     border
@@ -44,12 +44,9 @@
             }
         },
         methods: {
-            check: function () {
-                alert('An event broke out');
-            },
             EventTest: function (val) {
                 alert(`occured ${val}`);
-            }
+            },
         }
     }
 
