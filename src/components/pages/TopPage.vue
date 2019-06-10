25408c2f9e2c9c1e92329dcd764be2f271842cd6
kzmaro <maronkazu94@gmail.com>
2019-06-10 23:36:04 +0900

    TopPageの追加

---
+++ src/components/pages/TopPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <H1>This is Top Page!!</H1>
+        <v-btn color="green">use this</v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TopPage"
+    }
+</script>
+
+<style scoped>
+</style>
