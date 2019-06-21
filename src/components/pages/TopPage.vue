030527c0ce3e447e1bb8de79b449e6eaa22c1d46
Kazuma Kosuge <kosu.1118.git@gmail.com>
2019-06-21 14:53:07 +0900

    ボックスの振り分けの構想

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,8 +1,23 @@
 <template>
     <div>
-        <H1>Welcome Our System!!</H1>
-        <p1>大学授業逆評価システムへようこそ!!<br></p1>
-        <v-btn color="red">use this</v-btn>
+        <div class="box1">
+            <header>
+                <h1>授業評価</h1>
+                    <h2 align="center">～授業選びに迷ったあなたに～</h2>
+            </header>
+            <p align="center">大学授業逆評価システムへようこそ!!<br></p>
+        </div>
+        <div class="vtn-box">
+             <v-btn color="red">use this</v-btn>
+        </div>
+        <div class="box2">
+            <ul>
+                <p>以下のご希望に答えます。</p>
+                <li>楽な授業が知りたい</li>
+                <li>気になる授業の内容が知りたい</li>
+                <li>時間割決めで失敗したくない</li>
+            </ul>
+        </div>    
     </div>
 </template>
 
@@ -14,12 +29,22 @@
 
 <style scoped>
 div{
-    background-color: burlywood;
-    height: 640px;
+    color: white;
 }
-p1{
-    font-size: 20px;
+div .box1{
+    background: black;
+    width: auto;
+}
+div .vtn-box{
     text-align: center;
+    height: 65px;
+}
+div .box2{
+    border: none;
+    background: linear-gradient(0deg, brown, black);
+}
+h1{
+    padding: 5px;
 }
 
 </style>
