4919edd06b417eae4e69de95cab6b185508da4c8
Kazuma Kosuge <kosu.1118.git@gmail.com>
2019-06-23 00:25:22 +0900

    全体的なイメージはOK

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,19 +1,30 @@
 <template>
-    <div class="container" style="background-image: url(./img/resize.jpg);">
-        <div class="link-box">
-            <h1>タイトル</h1>
-        </div>
-        <div class="title">
-            <h1 align="center">タイトル</h1>
-                <p align="center">サブタイトル</p>
-        </div>
-        <div class="btn-box">
-            <v-btn color="blue">use this</v-btn>
-        </div>
-        <div class="box2">
+    <body>
+        <div class="container">
+            <ul id="nav">
+                <li><a href="">Home</a></li>
+                <li><a href="">トップ</a></li>
+            </ul>
             
-        </div>    
-    </div>
+            <h1 align="center">大学授業支援システム</h1>
+            <div align="center" class="border-parent">
+                <p class="border"></p>
+            </div>
+            <p>
+            自分の大学のこの授業の内容が知りたい。<br>
+            そんなときは、ここで探しましょう。
+            </p>
+            <div class="btn-box">
+                <v-btn color="blue">さっそくはじめる</v-btn>
+            </div>
+        </div>
+        <div class="black">
+            <ul>
+                <li><a href="">kosu.1118.git@gmail.com</a></li>
+            </ul>
+        </div>
+    </body>
+    
 </template>
 
 <script>
@@ -23,25 +34,60 @@
 </script>
 
 <style scoped>
-div{
-    color: white;
-}
 .container{
-    height: auto;
-    width: auto;
-}
-div .link-box{
-    width: 100%;
-    height: 20%;
-}
-div .btn-box{
-    text-align: center;
-}
-div .box2{
-    border: none;
+    background-image: url("./img/resize.jpg");
+    background-size: cover;
 }
 h1{
-    padding: 5px;
+    font-size: 36px;
+    margin-top: 10%;
+    margin-bottom: 20px;
 }
-
+.border{
+    width: 75%;
+    height: 25px;
+    border-top: solid 5px rgba(0, 0, 3, 0.584);
+}
+p{
+    text-align: center;
+    font-size: 20px;
+}
+.btn-box{
+    text-align: center;
+    margin-top: 15%;
+}
+.nav-box{
+    margin-bottom: 25%;
+}
+#nav{
+    list-style: none;
+    overflow: hidden;
+    width: 100%;
+}
+#nav li{
+    width: 48%;
+    text-align: center;
+    background-color: #333;
+    float: left;
+    height: 50px;
+    line-height: 50px;
+    margin-right: 2px;
+    text-align: center;
+}
+#nav li a{
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+    padding: 20px;
+}
+div .black{
+    background-color: black;
+}
+.black ul li{
+    list-style: none;
+    text-align: center;
+    font-size: 20px;
+    color: white;
+    padding: 20px;
+}
 </style>
