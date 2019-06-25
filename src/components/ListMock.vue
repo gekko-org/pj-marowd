cdd73272c495c05a7715d12ec9aeaa7f0d8a4fab
kzmaro <maronkazu94@gmail.com>
2019-06-25 11:24:31 +0900

    ListMock作成途中commit

---
+++ src/components/ListMock.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <app-filter-search @input="EventTest"></app-filter-search>
+        <ul v-for="i in 2">
+            <app-class-summary
+                    title="アドラー心理学基礎"
+                    faculty="心理"
+                    grade="1"
+                    professor="アルフレッド・アドラー"
+                    :is-random="true"
+                    department="個人心理"
+                    :rating="4.76"
+                    last-updated-by="reud"
+                    term="autumn"
+                    @click="EventTest">
+            </app-class-summary>
+        </ul>
+        <app-class-summary
+                title="アドラー心理学基礎"
+                faculty="心理"
+                grade="1"
+                professor="アルフレッド・アドラー"
+                :is-random="true"
+                department="個人心理"
+                :rating="4.76"
+                last-updated-by="reud"
+                term="all"
+                @click="EventTest">
+        </app-class-summary>
+        <ul v-for="i in 3">
+            <app-class-summary
+                    title="アドラー心理学基礎"
+                    faculty="心理"
+                    grade="1"
+                    professor="アルフレッド・アドラー"
+                    :is-random="true"
+                    department="個人心理"
+                    :rating="4.76"
+                    last-updated-by="reud"
+                    term="spring"
+                    @click="EventTest">
+            </app-class-summary>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import AppFilterSearch from "@/components/AppFilterSearch";
+    import AppClassSummary from "@/components/AppClassSummary";
+
+    export default {
+        name: "ListMock",
+        components: {AppClassSummary, AppFilterSearch},
+
+        data: function () {
+            return {}
+        },
+        methods: {
+            EventTest: function (val) {
+                alert(`occured ${val}`);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import Vuetify from 'vuetify'
-import TopPage from './components/pages/TopPage.vue';
+// import TopPage from './components/pages/TopPage.vue';
+import ListMock from './components/ListMock.vue';
 
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 new Vue({
-  render: h => h(TopPage),
+    render: h => h(ListMock),
 }).$mount('#app')
 
 new Vue({
-  el: '#apps',
-  data: {},
-  components: {
-  }
+    el: '#apps',
+    data: {},
+    components: {}
 });
