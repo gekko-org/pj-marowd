3645fb0f1d74a669923cdb996ce8f72bbfb92534
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-06-30 20:59:48 +0900

    WIP: #36 やること: ログインページにリダイレクトさせてトークンをVuexに保存してそこからログイン状態の判別を行う

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <div v-if="loggedIn" class='namestyle'>{{username}}</div>
-        <v-btn color="info" v-if="loggedIn" v-on:click=Test(loggedIn)>Logout</v-btn>
-        <v-btn color="info" v-if="!loggedIn" v-on:click=Test(loggedIn)>Login</v-btn>
+        <v-btn color="info" v-if="currentUser" v-on:click=logout>Logout</v-btn>
+        <v-btn color="info" v-else v-on:click=login>Login</v-btn>
     </v-toolbar>
 
     <v-content>
@@ -18,25 +18,66 @@
 
 <script>
     import ShowCase from "@/components/ShowCase";
-
+    import firebase from 'firebase';
 
     export default {
         name: 'App',
         components: {
             ShowCase,
         },
+        props: {
+            currentUser: {
+                type: firebase.User,
+                default: null
+            },
+            unsubscribe: {
+                type: Function
+            }
+        },
         data() {
             return {
                 // loggedIn: getLoginState(),みたいに直接ログイン状態を判断して結果を埋め込む
                 //今はとりあえずそのまま値を入れている。
-                loggedIn: true,
-                username: 'takechan'
+                username: 'this.currentUser.displayName',
             }
         },
         methods: {
             Test: function (loginState) {
                 alert('Change  Login ' + loginState + ' to ' + !loginState);
             },
+            login: function() {
+                const provider = new firebase.auth.GoogleAuthProvider();
+                firebase.auth().signInWithPopup(provider).then(function(result) {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    var token = result.credential.accessToken;
+                    // The signed-in user info.
+                    var user = result.user;
+                    // ...
+                }).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                });
+            },
+            logout: function () {
+                firebase.auth().signOut().then().catch(
+                    (err) => alert(err)
+                )
+            }
+        },
+        mounted () {
+            this.unsubscribe = firebase.auth().onAuthStateChanged(user => {
+                if (user) {
+                    this.currentUser = user
+                } else {
+                    this.currentUser = null
+                }
+            })
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,28 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import Vuetify from 'vuetify'
 import App from './App'
+import firebase from 'firebase'
 
-Vue.config.productionTip = false
-Vue.use(Vuetify)
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCTUDGs3SAzmBQUOv-kQq7s_PYaWOpdio",
+    authDomain: "pj-marowd.firebaseapp.com",
+    databaseURL: "https://pj-marowd.firebaseio.com",
+    projectId: "pj-marowd",
+    storageBucket: "pj-marowd.appspot.com",
+    messagingSenderId: "878555019849",
+    appId: "1:878555019849:web:22d4242096f12135"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
-  el: '#apps',
-  data: {},
-  components: {
-  }
+    el: '#apps',
+    data: {},
+    components: {}
 });
