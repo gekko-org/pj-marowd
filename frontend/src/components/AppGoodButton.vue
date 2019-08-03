b323c824bbbe886b7a5cb7b51905a0650cf2d18d
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-08-03 21:15:05 +0900

    move _files

---
+++ frontend/src/components/AppGoodButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn tile @click="stateChange">
+    <v-icon left color="pink">mdi-info</v-icon>{{ favAmount }}
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+@Component
+export default class AppGoodButton extends Vue {
+  @Prop({ required: true })
+  public favAmount!: number;
+  public isPushed: boolean = false;
+  stateChange() {
+    this.isPushed ? this.favAmount-- : this.favAmount++;
+    this.isPushed
+      ? this.favUnPush(this.favAmount)
+      : this.favPush(this.favAmount);
+    this.isPushed = !this.isPushed;
+  }
+  @Emit()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  favPush(val: number) {}
+  @Emit()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  favUnPush(val: number) {}
+}
+</script>
+
+<style></style>
