3442cd75124364a992344b0e9b20c4a2ea70e4aa
kzmaro <maronkazu94@gmail.com>
2019-08-06 14:06:31 +0900

    アカウント削除の時の処理を追加

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -10,12 +10,19 @@ const ref = db.ref("server/account-data/");
 export const WelcomeLog = functions.auth.user().onCreate((user) => {
     console.log('Hello ' + user.displayName + ' logged in' + 'called by TS');
 
-    // データベースに書き込む。一意に定まるユーザのuidを主キーとして設定し、メアドと名前を格納する
+    // データベースに書き込む。一意に定まるユーザのuidを主キーとして設定し、メアドと名前を格納する。
     ref.child('users/' + user.uid).set({
         mail: user.email,
         name: user.displayName,
     });
+    return 0;
 });
 
+export const DeleteLog = functions.auth.user().onDelete((user) => {
+    console.log('Hello ' + user.displayName + ' account deleted ' + 'called by TS');
 
+    ref.child('users/' + user.uid).remove();
+
+    return 0;
+});
 
