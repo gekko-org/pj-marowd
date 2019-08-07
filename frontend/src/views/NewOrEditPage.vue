d0fabbc739ae853938ca4e189e5b532da31d04dd
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-08-07 20:24:29 +0900

    NewOrEditPageのTS化

--- frontend/src/views/NewOrEditPage.vue
+++ frontend/src/views/NewOrEditPage.vue
@@ -6,10 +6,9 @@
       <v-form ref="form">
         <v-text-field
           v-model="name"
-          :error-messages="nameErrors"
           label="科目名"
-          @input="$v.name.$touch()"
-          @blur="$v.name.$touch()"
+          :rules="rules"
+          required
         ></v-text-field>
       </v-form>
     </v-flex>
@@ -17,7 +16,7 @@
       <v-flex xs6 md3 class="pa-2">
         <v-select
           v-model="dep"
-          :items="departmentItems"
+          :items="departments"
           label="学部"
         ></v-select>
       </v-flex>
@@ -25,7 +24,7 @@
         <v-select v-model="major" :items="majorItems" label="学科"></v-select>
       </v-flex>
       <v-flex xs6 md3 class="pa-2">
-        <v-select v-model="year" :items="yearSelect" label="学年"></v-select>
+        <v-select v-model="year" :items="yearSelects" label="学年"></v-select>
       </v-flex>
       <v-flex xs6 md3 class="pa-2">
         <v-form ref="form">
@@ -41,77 +40,68 @@
         color="indigo"
         label="抽選授業"
       ></v-checkbox>
-      <v-btn color="info" @click="submit">授業を追加する</v-btn>
+      <v-btn color="info" :disabled="!valid" @click="submit">授業を追加する</v-btn>
     </v-layout>
   </v-container>
 </template>
 
-<script>
-import { required } from 'vuelidate/lib/validators';
-import { validationMixin } from 'vuelidate';
+<script lang="ts">
 
-export default {
-  mixins: [validationMixin],
-  name: 'AppNewOrEditPage',
-  validations: {
-    name: {
-      required
-    }
-  },
-  data: function() {
-    return {
-      name: '',
-      dep: 'foo',
-      year: '',
-      major: '',
-      teacher: '',
-      lottery: ''
-    };
-  },
-  updated: function() {
-    this.$emit('input', this.major);
-  },
-  methods: {
-    submit() {
-      if (!this.$v.$invalid) {
-        alert('submit is pushed!');
-        //ここに科目追加後の処理を記入する
-      } else {
-        this.$v.$touch();
-      }
-    }
-  },
-  props: {
-    departmentItems: {
-      type: Array,
-      default: () => ['理工', '生命', '情科', '共通']
-    },
-    yearSelect: {
-      type: Array,
-      default: () => ['1', '2', '3', '4', '共通', '大学院']
-    }
-  },
-  computed: {
-    majorItems: function() {
-      if (this.dep === '理工') {
-        return ['機械工', '電電', '応情'];
-      } else if (this.dep === '生命') {
-        return ['応植', '環応'];
-      } else if (this.dep === '情科') {
-        return ['CS', 'DM'];
-      } else {
-        return [''];
-      }
-    },
 
-    nameErrors: function() {
-      const errors = [];
-      if (!this.$v.name.$dirty) return errors;
-      !this.$v.name.required && errors.push('科目名が必要です');
-      return errors;
+
+import { Component, Vue, Emit } from 'vue-property-decorator';
+
+@Component
+export default class NewOrEditPage extends Vue {
+
+  public name: string = '';
+  public dep: string = 'foo';
+  public year: string = '';
+  public major: string = '';
+  public teacher: string = '';
+  public lottery: string = '';
+  public departments: string[] =  ['理工', '生命', '情科', '共通'];
+  public yearSelects: string[] = ['1', '2', '3', '4', '共通', '大学院'];
+  @Emit()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  input(val: string) {}
+
+  updated() {
+    this.input(this.major);
+  }
+  public static ruleDoNotEmpty(s: string): boolean {
+    return !!s;
+  }
+  public get rules(): Array<(v: string) => any> {
+    // いつかルール追加するかも・・・？
+    const rules: Array<(v: string) => any> = [];
+    const doNotEmptyRUle = (v: string) =>
+            NewOrEditPage.ruleDoNotEmpty(v) || 'Do Not Empty';
+    rules.push(doNotEmptyRUle);
+    return rules;
+  }
+
+  get majorItems(): string[] {
+    if (this.dep === '理工') {
+      return ['機械工', '電電', '応情'];
+    } else if (this.dep === '生命') {
+      return ['応植', '環応'];
+    } else if (this.dep === '情科') {
+      return ['CS', 'DM'];
+    } else {
+      return [''];
     }
   }
-};
+
+  submit() {
+    alert('submitted!');
+  }
+  public get valid(): boolean {
+    return !!this.name;
+  }
+}
+
+
 </script>
 
 <style scoped></style>
