854a184ef97d1457da44bad41d13be5ca5b7e39d
kzmaro <maronkazu94@gmail.com>
2019-08-08 22:40:48 +0900

    lint

--- frontend/src/components/AppGoodButton.vue
+++ frontend/src/components/AppGoodButton.vue
@@ -6,36 +6,38 @@
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue, Emit} from 'vue-property-decorator';
-    @Component
-    export default class AppGoodButton extends Vue {
-        @Prop({required: true})
-        public favAmount!: number;
-        public isPushed: boolean = false;
-        @Prop({ default: false })
-        public isBorder!: boolean;
+  import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
 
-        stateChange() {
-            this.isPushed ? this.favAmount-- : this.favAmount++;
-            this.isPushed
-                ? this.favUnPush(this.favAmount)
-                : this.favPush(this.favAmount);
-            this.isPushed = !this.isPushed;
-        }
-        @Emit()
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        favPush(val: number) {
-        }
+  @Component
+  export default class AppGoodButton extends Vue {
+    @Prop({ required: true })
+    public favAmount!: number;
+    public isPushed: boolean = false;
+    @Prop({ default: false })
+    public isBorder!: boolean;
 
-        @Emit()
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        favUnPush(val: number) {
-        }
+    stateChange() {
+      this.isPushed ? this.favAmount-- : this.favAmount++;
+      this.isPushed
+        ? this.favUnPush(this.favAmount)
+        : this.favPush(this.favAmount);
+      this.isPushed = !this.isPushed;
     }
+
+    @Emit()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    favPush(val: number) {
+    }
+
+    @Emit()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    favUnPush(val: number) {
+    }
+  }
 </script>
 
 <style>
-    .waku{
+    .waku {
         border: solid 2px #000000;
     }
 </style>
