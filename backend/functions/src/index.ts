761720320c6ea18fd12ed755ccd8e2ef888e8afc
kzmaro <maronkazu94@gmail.com>
2019-11-17 10:11:51 +0900

    バックエンド関数作成途中

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,28 +1,156 @@
 import * as functions from 'firebase-functions';
 
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.database();
-const ref = db.ref("server/account-data/");
+const fdb = admin.firestore();
+
+const ref = db.ref('server/account-data/');
 
 
 export const WelcomeLog = functions.auth.user().onCreate((user) => {
-    console.log('Hello ' + user.displayName + ' logged in' + 'called by TS');
+  console.log('Hello ' + user.displayName + ' logged in' + 'called by TS');
 
-    // データベースに書き込む。一意に定まるユーザのuidを主キーとして設定し、メアドと名前を格納する。
-    ref.child('users/' + user.uid).set({
-        mail: user.email,
-        name: user.displayName,
-    });
-    return 0;
+  // データベースに書き込む。一意に定まるユーザのuidを主キーとして設定し、メアドと名前を格納する。
+  ref.child('users/' + user.uid).set({
+    mail: user.email,
+    name: user.displayName
+  });
+  return 0;
 });
 
 export const DeleteLog = functions.auth.user().onDelete((user) => {
-    console.log('Hello ' + user.displayName + ' account deleted ' + 'called by TS');
+  console.log('Hello ' + user.displayName + ' account deleted ' + 'called by TS');
 
-    ref.child('users/' + user.uid).remove();
+  ref.child('users/' + user.uid).remove();
 
-    return 0;
+  return 0;
 });
 
+//　DBに登録or削除する処理はここまで
+//　ここから下はswaggerに記載した処理を行う
+
+// クライアントから送られてきたIDトークンを元に処理を行う
+
+// request.body.name　で送られてきたリクエストにアクセスできる。
+export const GetComment = functions.https.onRequest((request, response) => {
+
+  // const comment = fdb.collection('ClassSummary').doc(request.query['class_name']).collection('comment').doc('1');
+  // const comment = fdb.collection('ClassSummary').doc(request.query['class_name']);
+  // console.log(request.query['class_name'],request.query['comment_id']);
+  // console.log(comment);
+  const Ref = fdb.document('ClassSummary/{request.query[\'class_name\']}');
+  const doc = Ref.get();
+  if (doc.exists) {
+    console.log(doc.id);
+    console.log(doc.data());
+  } else {
+    console.log('error');
+  }
+  response.status(200);
+  // console.log(request.query['class_name'],request.query['comment_id']);
+});
+
+export const test = functions.https.onRequest((request, response) => {
+  const aTuringRef = fdb.collection('users').doc('aturing');
+  aTuringRef.set({
+    'first': 'Alan',
+    'middle': 'Mathison',
+    'last': 'Turing',
+    'born': 1912
+  });
+  response.status(200);
+  return 0;
+});
+
+export const testget = functions.https.onRequest((request, response) => {
+  fdb.collection('users').get()
+    .then((snapshot: { forEach: (arg0: (doc: any) => void) => void; }) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err: any) => {
+      console.log('Error getting documents', err);
+    });
+  return 0;
+});
+// request.query['class_name']
+export const comments = functions.https.onRequest((request, response) => {
+  // fdb.collection('ClassSummary').doc('最適化数学').collection('comment').get()
+  const class_query=request.query['class_name'];
+  console.log(class_query);
+  fdb.collection('ClassSummary').doc(class_query).collection('comment').get()
+    .then((snapshot: { forEach: (arg0: (doc: any) => void) => void; }) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+        response.send(doc.data());
+      });
+    })
+    .catch((err: any) => {
+      console.log('Error getting documents', err);
+      response.send(404)
+    });
+  return 0;
+});
+
+export const testget3 = functions.https.onRequest((request, response) => {
+  const eee=fdb.collection('ClassSummary').doc('最適化数学').collection('comment').doc('1');
+  console.log(eee.data());
+  // console.log(ddd.id, '=>', ddd.data());
+  console.log(request.query['class_name'],request.query['comment_id']);
+
+  return 0;
+});
+
+export const testget4 = functions.https.onRequest((request, response) => {
+  console.log(request.query['class_name'],request.query['comment_id']);
+  return 0;
+});
+
+export const testget5 = functions.https.onRequest((request, response) => {
+  const data= fdb.collection('ClassSummary').doc('最適化数学').collection('comment').where('comment', '==', request.query['comment_id']).get();
+  console.log(data.id);
+  console.log(data.data());
+  response.send(data.data());
+  return 0;
+});
+
+export const testget6 = functions.https.onRequest((request, response) => {
+  const data= fdb.collection('ClassSummary').doc('最適化数学').where('comment', '==', request.query['comment_id']).get();
+  console.log(data.id);
+  console.log(data.data());
+  response.send(data.data());
+  return 0;
+});
+
+export const testget7 = functions.https.onRequest((request, response) => {
+  const data= fdb.collection('ClassSummary').doc('最適化数学').collection('comment').where('comment', '==', 1).get();
+  console.log(data.id);
+  console.log(data.data());
+  response.send(data.data());
+  return 0;
+});
+
+export const comments2 = functions.https.onRequest((request, response) => {
+  // fdb.collection('ClassSummary').doc('最適化数学').collection('comment').get()
+  const jsonStr = '{"Comments":[]}';
+  const obj = JSON.parse(jsonStr);
+  const class_query=request.query['class_name'];
+  console.log(class_query);
+  fdb.collection('ClassSummary').doc(class_query).collection('comment').get()
+    .then((snapshot: { forEach: (arg0: (doc: any) => void) => void; }) => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        obj['Comments'].push(doc.data());
+      });
+    })
+    .catch((err: any) => {
+      console.log('Error getting documents', err);
+    });
+  // @ts-ignore
+  jsonStr = JSON.stringify(obj);
+  response.send(jsonStr);
+  return 0;
+});
